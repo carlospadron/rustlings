2cb94a3e8a6b5a2bdd853db574851a60aa4a874d
carlos.padron <carlos.padron@gigaclear.com>
2024-06-10 09:32:08 +0100

    traits

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -10,16 +10,20 @@
 
 // I AM NOT DONE
 
+//i'd have preferred to modify the structs to take a parameter and modify the implementation
+// of the trait to use the parameter
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 struct SomeSoftware {
-    version_number: i32,
+    version_number: i32
 }
 
 struct OtherSoftware {
-    version_number: String,
+    version_number: String
 }
 
 impl Licensed for SomeSoftware {} // Don't edit this line
@@ -32,9 +36,10 @@ mod tests {
     #[test]
     fn is_licensing_info_the_same() {
         let licensing_info = String::from("Some information");
-        let some_software = SomeSoftware { version_number: 1 };
+        let some_software = SomeSoftware { 
+            version_number: 1};
         let other_software = OtherSoftware {
-            version_number: "v2.0.0".to_string(),
+            version_number: "v2.0.0".to_string()
         };
         assert_eq!(some_software.licensing_info(), licensing_info);
         assert_eq!(other_software.licensing_info(), licensing_info);
